db920e84e7adbc77e150271db0d7ff3b4715357c
Christian Lessa <christianclessa@gmail.com>
2022-03-22 13:39:35 -0300

    Criando o endpoint get /talker

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs').promises;
+
+const talkers = 'talker.json';
 
 const app = express();
 app.use(bodyParser.json());
@@ -10,6 +13,15 @@ const PORT = '3000';
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+app.get('/talker', async (req, res) => {
+  try {
+    const fileTalkers = await fs.readFile(talkers, 'utf8');
+    res.status(200).send(JSON.parse(fileTalkers));
+  } catch (err) {
+    console.error(`Erro ao ler o arquivo: ${err.message}`);
+  }
 });
 
 app.listen(PORT, () => {
