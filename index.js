e81704800c47bfd0f10339878d01084cebf36d29
Christian Lessa <christianclessa@gmail.com>
2022-03-22 15:11:26 -0300

    Criando o endpoint GET /talker/:id

--- index.js
+++ index.js
@@ -24,6 +24,16 @@ app.get('/talker', async (req, res) => {
   }
 });
 
+app.get('/talker/:id', async (req, res) => {
+  const { id } = req.params;
+  const fileTalkers2 = await fs.readFile(talkers, 'utf8').then((data) => JSON.parse(data));
+
+  const talkerID = fileTalkers2.find((talk) => talk.id === id);
+  if (!talkerID) return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+
+  res.status(200).json(talkerID);
+});
+
 app.listen(PORT, () => {
   console.log('Online');
 });
